36d699805094f4f3be6b328f3766ddb2abf414f7
Nikhil Joshi <najoshi@login2.hive.hpc.ucdavis.edu>
2026-03-12 16:15:53 -0700

    adding mouse genome and rrna

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 STAR_REF_HUMAN_DIR="/quobyte/bioinfocore-grp/genomes/human/GRCh38.p14/STAR_2.7.11b_index"
 STAR_GTF_HUMAN_FILE="/quobyte/bioinfocore-grp/genomes/human/GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.gtf"
-RRNA_HUMAN_FILE=""
+RRNA_HUMAN_FILE="/quobyte/bioinfocore-grp/genomes/human/GRCh38.p14/GCF_000001405.40_GRCh38.p14_genomic.gtf"
 
-STAR_REF_MOUSE_DIR=""
-STAR_GTF_MOUSE_FILE=""
-RRNA_MOUSE_FILE=""
+STAR_REF_MOUSE_DIR="/quobyte/bioinfocore-grp/genomes/mouse/GRCm39/STAR_GRCm39_vM38"
+STAR_GTF_MOUSE_FILE="/quobyte/bioinfocore-grp/genomes/mouse/GRCm39/gencode.vM38.basic.annotation.gtf"
+RRNA_MOUSE_FILE="/quobyte/bioinfocore-grp/genomes/mouse/GRCm39/rRNA_mouse_03-12-2026.fasta"
 
 SAMPLE_FILE="sample_data.txt"
 
