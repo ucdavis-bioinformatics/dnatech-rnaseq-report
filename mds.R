f73ecfbc04f18620bacd1e8aacc5d054a25ca975
Nikhil Joshi <najoshi@login2.hive.hpc.ucdavis.edu>
2026-03-16 19:51:38 -0700

    outputting html reports as well

--- mds.R
+++ mds.R
@@ -12,6 +12,11 @@ pdf("mds_all.pdf")
 plotMDS(dge, main="Multi-Dimensional Scaling Plot for all counts")
 dev.off()
 
+jpeg("mds_all.jpg")
+# MDS plot
+plotMDS(dge, main="Multi-Dimensional Scaling Plot for all counts")
+dev.off()
+
 
 
 counts <- read.table("htseq.dedup.counts.tsv", header=TRUE, row.names=1, check.names=FALSE)
@@ -22,6 +27,11 @@ dge <- DGEList(counts=counts)
 dge <- calcNormFactors(dge)
 
 pdf("mds_dedup.pdf")
+# MDS plot
+plotMDS(dge, main="Multi-Dimensional Scaling Plot for deduplicated counts")
+dev.off()
+
+jpeg("mds_dedup.jpg")
 # MDS plot
 plotMDS(dge, main="Multi-Dimensional Scaling Plot for deduplicated counts")
 dev.off()
