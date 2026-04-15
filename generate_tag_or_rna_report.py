3bb4c101c0af830581b0e315c83d7fd9e941d105
Nikhil Joshi <najoshi@login1.hive.hpc.ucdavis.edu>
2026-04-14 23:04:31 -0700

    adding tag seq

--- generate_tag_or_rna_report.py
+++ generate_tag_or_rna_report.py
@@ -165,12 +165,12 @@ print("Copying scripts, templates, and config...")
 if tag_or_rna == "rna":
 
     os.system(f"cp rnaseq_pe.slurm htseq_multiqc.slurm mds.R multiqc_config_pdf.yaml {outputdir}")
-    os.system(f"cp biocore_banner.png final_report_rnaseq.html mds_plots.html {outputdir}/report_dir/")
+    os.system(f"cp biocore_banner.png final_report_rnaseq.html mds_plots_rnaseq.html {outputdir}/report_dir/")
     analysis_script = "rnaseq_pe.slurm"
 
 else:
     os.system(f"cp tagseq.slurm htseq_multiqc.slurm mds.R multiqc_config_pdf.yaml {outputdir}")
-    os.system(f"cp biocore_banner.png final_report_tagseq.html mds_plots.html {outputdir}/report_dir/")
+    os.system(f"cp biocore_banner.png final_report_tagseq.html mds_plot_tagseq.html {outputdir}/report_dir/")
     analysis_script = "tagseq.slurm"
 
 if mhcheck == "n":
